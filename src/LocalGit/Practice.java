fc4a41f7a087a7686b0f2f11ebb959a9bfd9426d
BalikAlex <97114939+BalikAlex@users.noreply.github.com>
2022-01-04 20:09:58 -0500

    Update Practice.java
    no changes

--- src/LocalGit/Practice.java
+++ src/LocalGit/Practice.java
@@ -5,9 +5,9 @@ public class Practice {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("this is old changes");
+		System.out.println("this is new changes");
 		
-		
+		//no changes
 		
 
 	}
