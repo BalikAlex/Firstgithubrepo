0f5239b32bb9d04bef7460b21de233a82902c200
BalikAlex <97114939+BalikAlex@users.noreply.github.com>
2022-01-04 19:52:37 -0500

    Update GitHub.java
    this is for practice

--- src/LocalGit/GitHub.java
+++ src/LocalGit/GitHub.java
@@ -3,11 +3,14 @@ package LocalGit;
 public class GitHub {
 public static void main(String[] args) {
 	
-	System.out.println("Hello");
+	System.out.println("hi there");
 	
 	
 }
 	public static void methodA() {
 		System.out.println("new update");
+	}
+	public void methodB() {
+		// this is second method for practice
 	}
 }
