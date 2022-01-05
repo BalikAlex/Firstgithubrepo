98a45c1111aac230228d56486ca8c52138ae20c6
BalikAlex <balic.alexandru@gmail.com>
2022-01-04 21:41:21 -0500

    example 1

---
+++ src/LocalGit/branchExample.java
@@ -0,0 +1,13 @@
+package LocalGit;
+
+public class branchExample {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public void testA() {
+		System.out.println("This is a example!!!");
+	}
+	
+}
